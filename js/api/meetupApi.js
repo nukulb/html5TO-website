492fcb79b2e684b65a623ffc32d548f490078bf7
unknown <mspendlove@TN-mspendlove-3.corp.infusiondev.com>
2012-02-25 14:10:25 -0500

    Now getting events and outputting to test page

--- js/api/meetupApi.js
+++ js/api/meetupApi.js
@@ -3,40 +3,34 @@ meetupApi = {
     key: "73a1413161e5d5f525618178644773",
 	getMeetupEvents: function (callback) {
 		
-		var url = "https://api.meetup.com/2/events/?key=" + meetupApi.key + "&sign=true&status=past&group_id=1757714&page=20";
+		var url = "https://api.meetup.com/2/events/?key=" + meetupApi.key + "&sign=true&status=past&group_id=1757714&page=20&callback=?";
 	
 		function parseResults (data) {
 			
 			var results = [];
 			
+			//Iterate through results and return simpler objects as an array
 			if (data && data.results && data.results.length > 0)
 			{
-				for( var i in data) {
+				for( var i in data.results) {
 					
 					var temp = {};
 					
-					temp.name = data[i].name;
-					temp.time = data[i].time;
-					temp.url = data[i].event_url;
-					temp.description = data[i].description;
-					
+					temp.name = data.results[i].name;
+					temp.time = data.results[i].time;
+					temp.url = data.results[i].event_url;
+					temp.description = data.results[i].description;
 					results.push(temp);
 				}
 			}
-			
+
 			return results;
-			
 		}
 	
-		$.get(url, 
-			'json',
-			function(data, textStatus, jqXHR) {
-				var parsedResults = parseResults(data);
-				callback(data);
-			},
-			function(jqXHR, textStatus, errorThrown) {
-				alert("There was an error:" + errorThrown );
-			});
+		$.getJSON(url, function (data) {
+			var results = parseResults(data);
+			callback(results);
+		});
 		
 	}
  
