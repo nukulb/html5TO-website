7e1c546583e52c85a84e063a82ac779615a28f61
unknown <Regan@.(none)>
2012-02-25 12:01:31 -0500

    additional functions

--- js/api/meetupApi.js
+++ js/api/meetupApi.js
@@ -2,10 +2,16 @@ meetupApi = {
     
     var key = "73a1413161e5d5f525618178644773";
 
-    getMeetupEvents = function () {
+    getNextEvent = function () {
     
     }
+	
+	getMeetupMembers = function () {
+		// https://api.meetup.com/members?key=73a1413161e5d5f525618178644773&sign=true&group_urlname=HTML5-Web-App-Developers&page=20
+	}
 
-
+	getPastEvents = function () {
+		
+	}
 
 };
