0c73a17b7f83717519d95d21cc55788609249bd6
unknown <mspendlove@TN-mspendlove-3.corp.infusiondev.com>
2012-02-25 13:07:52 -0500

    merged.

--- js/api/meetupApi.js
+++ js/api/meetupApi.js
@@ -1,17 +1,45 @@
 meetupApi = {
-    
-    var key = "73a1413161e5d5f525618178644773";
-
-    getNextEvent = function () {
-    
-    }
+   
+    key: "73a1413161e5d5f525618178644773",
+ 
+    getMeetupEvents: function (callback) {
+		alert("hi");
+		/*
+		var url = "https://api.meetup.com/2/events/?key=" + meetupApi.key + "&sign=true&status=past&group_id=1757714&page=20";
 	
-	getMeetupMembers = function () {
-		// https://api.meetup.com/members?key=73a1413161e5d5f525618178644773&sign=true&group_urlname=HTML5-Web-App-Developers&page=20
+		function parseResults (data) {
+			
+			var results = [];
+			
+			if (data && data.results && data.results.length > 0)
+			{
+				foreach( var i in data) {
+					
+					var temp = {};
+					
+					temp.name = data[i].name;
+					temp.time = data[i].time;
+					temp.url = data[i].event_url;
+					temp.description = data[i].description;
+					
+					results.push(temp);
+				}
+			}
+			
+			return results;
+			
+		}
+	
+		$.get(url, 
+			'json',
+			success:  function(data, textStatus, jqXHR) {
+				
+				var parsedResults = parseResults(data);
+				callback(data);
+			},
+			error: function(jqXHR, textStatus, errorThrown) {
+				alert("There was an error:" + errorThrown );
+			});
+			*/
 	}
-
-	getPastEvents = function () {
-		
-	}
-
 };
